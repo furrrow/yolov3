ca1e563765ee576c875ba1b76de1fe07da5ac72d
furrrow <28491614+furrrow@users.noreply.github.com>
2022-12-27 11:28:09 -0500

    getting started with model.py

--- model.py
+++ model.py
@@ -1,32 +1,10 @@
 import torch
 import torch.nn as nn
 
-v1_config = [
-    # Tuple: (kernel_size, num_filters, stride, padding)
-    # List: [tuples*, repeats]
-    (7, 64, 2, 3),
-    "M",
-    (3, 192, 1, 1),
-    "M",
-    (1, 128, 1, 0),
-    (3, 256, 1, 1),
-    (1, 256, 1, 0),
-    "M",
-    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
-    (1, 512, 1, 0),
-    (3, 1024, 1, 1),
-    "M",
-    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
-    (3, 1024, 1, 1),
-    (3, 1024, 2, 1),
-    (3, 1024, 1, 1),
-    (3, 1024, 1, 1)
-]
-
 # tuple: (out_channels, kernel_size, stride)
 # everything in yolov3 uses the same padding
 # List: ["B" = res blocks, repeat]
-v3_config = [
+config = [
     (32, 3, 1),
     (64, 3, 2),
     ["B", 1],
@@ -55,58 +33,125 @@ v3_config = [
 
 
 class CNNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, **kwargs):
-        super(CNNBlock, self).__init__()
+    def __init__(self, in_channels, out_channels, bn_act=True, **kwargs):
+        super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.leakyrelu = nn.LeakyReLU(0.1)
+        self.use_bn_act = bn_act
 
     def forward(self, x):
-        return self.leakyrelu(self.batchnorm(self.conv(x)))
+        if self.use_bn_act:
+            return self.leakyrelu(self.batchnorm(self.conv(x)))
+        else:
+            return self.conv(x)
 
 
-class YoloV1(nn.Module):
-    def __init__(self, in_channels=3, **kwargs):
-        super(YoloV1, self).__init__()
-        self.architecture = architecture_config
+class ResidualBlock(nn.Module):
+    def __int__(self, channels, use_residual=True, num_repeats=1):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for repeat in range(num_repeats):
+            self.layers += [
+                nn.Sequential(
+                    CNNBlock(channels, channels // 2, kernel_size=1),
+                    CNNBlock(channels // 2, channels, kernel_size=3, padding=1)
+                )
+            ]
+        self.use_residual = use_residual
+        self.num_repeats = num_repeats
+
+    def forward(self, x):
+        for layer in self.layers:
+            if self.use_residual:
+                x = x + layer(x)
+            else:
+                x = layer(x)
+
+        return x
+
+
+class ScalePrediction(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.pred = nn.Sequential(
+            CNNBlock(in_channels, 2 * in_channels, kernel_size=3, padding=1),
+            CNNBlock(2 * in_channels, (num_classes + 5) * 3, bn_act=False, kernel_size=1)
+        )
+
+        self.num_classes = num_classes
+
+    def forward(self, x):
+        return (
+            self.pred(x).reshape(x.shape[0], 3, self.num_classes + 5, x.shape[2], x.shape[3]).permute(0, 1, 3, 4, 2)
+        )
+
+
+class YoloV3(nn.Module):
+    def __init__(self, in_channels=3, num_classes=80):
+        super().__init__()
+        self.num_classes = num_classes
         self.in_channels = in_channels
-        self.darknet = self._create_conv_layers(self.architecture)
-        self.fc = self._create_fc(**kwargs)
+        self.layers = self._create_conv_layers()
 
     def forward(self, x):
-        x = self.darknet(x)
-        return self.fc(torch.flatten(x, start_dim=1))
+        outputs = []
+        route_connections = []
+        for layer in self.layers:
+            if isinstance(layer, ScalePrediction):
+                outputs.append(layer(x))
+                continue
+            x = layer(x)
+
+            if isinstance(layer, ResidualBlock) and layer.num_repeats == 8:
+                route_connections.append(x)
+            elif isinstance(layer, nn.Upsample):
+                x = torch.cat([x, route_connections[-1]], dim=1)
+                route_connections.pop()
+        return outputs
 
     def _create_conv_layers(self, architecture):
-        layers = []
+        layers = nn.ModuleList()
         in_channels = self.in_channels
 
-        for x in architecture:
-            if type(x) == tuple:
-                ks, num_filters, s, p = x
-                layers += [
-                    CNNBlock(in_channels, num_filters, kernel_size=ks, stride=s, padding=p)
-                ]
-                in_channels = num_filters
-            elif type(x) == str:
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            elif type(x) == list:
-                conv1, conv2, num_repeats = x
-                ks1, num_filters1, s1, p1 = conv1
-                ks2, num_filters2, s2, p2 = conv1
-                for _ in range(num_repeats):
-                    layers += [CNNBlock(in_channels, num_filters1, kernel_size=ks1, stride=s1, padding=p1)]
-                    layers += [CNNBlock(num_filters1, num_filters2, kernel_size=ks2, stride=s2, padding=p2)]
-                    in_channels = num_filters2
-        return nn.Sequential(*layers)
+        for module in config:
+            if isinstance(module, tuple):
+                out_channels, kernel_size, stride = module
+                layers.append(
+                    CNNBlock(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+                             padding=1 if kernel_size == 3 else 0)
+                )
+                in_channels = out_channels
 
-    def _create_fc(self, split_size, num_boxes, num_classes):
-        s, b, c = split_size, num_boxes, num_classes
-        return nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(1024 * s * s, 512),  # original paper had 4096
-            nn.Dropout(),
-            nn.LeakyReLU(),
-            nn.Linear(512, s * s * (c + b * 5))  # (s, s, 30) where c + b*5 = 30
-            )
+            elif isinstance(module, list):
+                num_repeats = module[1]
+                layers.append(
+                    ResidualBlock(in_channels, num_repeats=num_repeats)
+                )
+            elif isinstance(module, str):
+                if module == "S":
+                    layers += [
+                        ResidualBlock(in_channels, use_residual=False, num_repeats=1),
+                        CNNBlock(in_channels, in_channels // 2, kernel_size=1),
+                        ScalePrediction(in_channels // 2, num_classes=self.num_classes)
+                    ]
+                    in_channels = in_channels // 2
+                elif module == "U":
+                    layers.append(nn.Upsample(scale_factor=2))
+                    in_channels = in_channels * 3
 
+        return layers
+
+
+def main():
+    num_classes = 20
+    IMAGE_SIZE = 416
+    model = YoloV3(num_classes=num_classes)
+    x = torch.randn((2, 3, IMAGE_SIZE, IMAGE_SIZE))
+    out = model(x)
+    assert model(x)[0].shape == (2, 3, IMAGE_SIZE // 32, IMAGE_SIZE // 32, num_classes + 5)
+    assert model(x)[1].shape == (2, 3, IMAGE_SIZE // 16, IMAGE_SIZE // 16, num_classes + 5)
+    assert model(x)[2].shape == (2, 3, IMAGE_SIZE // 8, IMAGE_SIZE // 8, num_classes + 5)
+    print("Success!")
+
+main()
